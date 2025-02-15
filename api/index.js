3438510bb422346fbda6afc8a6143ddcb6f5aa54
codebyshoaib <shobbi800@gmail.com>
2025-02-15 18:14:08 +0500

    .

--- api/index.js
+++ api/index.js
@@ -13,10 +13,11 @@ const Post = require('./models/Post')
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost::4000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
